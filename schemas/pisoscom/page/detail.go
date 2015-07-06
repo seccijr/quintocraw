b72f6c2e9b8eb5f68d82085058d4c081233a5ade
Carlos Ignacio Pérez Sechi <external.z21csech@decathlon.com>
2015-07-06 19:22:55 +0200

    Half way Detail Page Parsing

--- schemas/pisoscom/page/detail.go
+++ schemas/pisoscom/page/detail.go
@@ -4,11 +4,19 @@ import (
 	"github.com/seccijr/quintocrawl/model"
 	"github.com/PuerkitoBio/goquery"
 	"errors"
+	"regexp"
 )
 
 const THUMB_SELECT = ".frame.slideShow img"
+const PHOTO_URL_PRE = "http://fotos.imghs.net/"
+const PHOTO_REGEXP = "(s|m|l|xl)"
 
-func getThumbnails(dom *goquery.Document) []model.ImgNode {
+func tSizePhotoUrl(url string, size string) string {
+	r := regexp.MustCompile(PHOTO_URL_PRE + PHOTO_REGEXP)
+	return r.ReplaceAllString(url, PHOTO_URL_PRE + size)
+}
+
+func getThumbs(dom *goquery.Document) []model.ImgNode {
 	var result []model.ImgNode
 
 	dom.Find(THUMB_SELECT).Each(func(i int, s *goquery.Selection) {
@@ -19,32 +27,43 @@ func getThumbnails(dom *goquery.Document) []model.ImgNode {
 	return result
 }
 
-func photFromThumbnail(thumbnail *model.ImgNode) (model.ImgNode, error) {
+func photoFromThumb(thumb *model.ImgNode) (model.ImgNode, error) {
 	photo := &model.ImgNode{}
-	if thumbnail.Src == "" {
-		return nil, errors.New("No source for thumbnail")
+	if thumb.Src == "" {
+		return nil, errors.New("No source for thumb")
 	}
+	photo.Src = tSizePhotoUrl(thumb.Src, "l")
 
-	return photo
+	return photo, nil
 }
 
 func getPhotos(dom *goquery.Document) ([]model.ImgNode, error) {
 	var result []model.ImgNode
 
 	if Has(dom, THUMB_SELECT) {
-		thumbnails := getThumbnails(dom)
-		for thumbnail := range thumbnails {
-			if photo, err := photFromThumbnail(&thumbnail); err == nil {
-				result = append(result, photo)
-			}
-		}
+		thumbs := getThumbs(dom)
+		result = getPhotosFromThumbs(thumbs)
 	} else {
-		return nil, errors.New("No photos nor thumbnails")
+		return nil, errors.New("No photos nor thumbs")
+	}
+
+	return result, nil
+}
+
+func getPhotosFromThumbs(thumbs []model.ImgNode) ([]model.ImgNode, error) {
+	var result []model.ImgNode
+
+	for thumb := range thumbs {
+		if photo, err := photoFromThumb(&thumb); err == nil {
+			result = append(result, photo)
+		}
 	}
 
 	return result, nil
 }
 
 func (doc *PCDoc) ParseDetail() {
+	thumbs := getThumbs(doc.dom)
+	photos, _ := getPhotosFromThumbs(thumbs)
 
 }
